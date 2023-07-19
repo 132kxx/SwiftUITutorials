3f2c0a2798bc7ec9ee6b78e1b14dae79dee5c677
132kxx <132kxx@gmail.com>
2023-07-19 22:28:30 +0900

    set timer

---
+++ SwiftUITutorials/MoveRect.swift
@@ -0,0 +1,27 @@
+//
+//  MoveRect.swift
+//  SwiftUITutorials
+//
+//  Created by kxx on 2023/07/14.
+//
+
+import SwiftUI
+
+struct MoveRect: View {
+    
+    @State private var colors: [Color] = [
+        .red, .blue, .brown, .purple, .yellow, .indigo, .black, .cyan, .gray
+    ]
+    
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+struct MoveRect_Previews: PreviewProvider {
+    static var previews: some View {
+        MoveRect()
+    }
+}
